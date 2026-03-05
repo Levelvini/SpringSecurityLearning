7a2cec275ea37bb77f5264e16683a5602c96b306
Levelvini <viniciusfc29@gmail.com>
2026-03-05 18:33:00 -0300

    feat: SecurityConfig created

---
+++ src/main/java/com/levelvini/security/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.levelvini.security.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .cors(cors -> cors.configure(http))
+                .sessionManagement(sessios -> sessios.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.dispatcherTypeMatchers(DispatcherType.ERROR)
+                        .permitAll().requestMatchers(HttpMethod.POST,"/auth/login")
+                        .permitAll().requestMatchers(HttpMethod.POST,"auth/register")
+                        .permitAll().anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
